086f7b6404c1344e20ecd14a156e51ef311e1ccd
tylerslaton <mtslaton1@gmail.com>
2024-03-08 14:19:16 -0500

    feat: update homepage with sample links
    Signed-off-by: tylerslaton <mtslaton1@gmail.com>

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="flex flex-col justify-center items-center min-h-screen">
-    <Search :isInvalid="isInvalid" :errorMessage="errorMessage" :placeholder="placeHolder" />
+    <h1 class="text-5xl font-bold mb-4">Explore GPTScript tools</h1>
+    <div class="flex flex-wrap justify-center">
+      <router-link to="/github.com/gptscript-ai/search" class="tile rounded bg-gray-200 p-4 m-2 text-2xlg">Search</router-link>
+      <router-link to="/github.com/gptscript-ai/vision" class="tile rounded bg-gray-200 p-4 m-2 text-2xlg">Vision</router-link>
+      <router-link to="/github.com/gptscript-ai/image-generation" class="tile rounded bg-gray-200 p-4 m-2 text-2xlg">Image Generation</router-link>
+      <router-link to="/github.com/gptscript-ai/rag" class="tile rounded bg-gray-200 p-4 m-2 text-2xlg">RAG</router-link>
+    </div>
   </div>
 </template>
 
