b27124c8b482df99d4779805dd4809807e9c638f
Vincent Fiduccia <vincent@acorn.io>
2024-03-21 10:28:39 -0700

    Fix accordion state

--- src/components/Accordion.vue
+++ src/components/Accordion.vue
@@ -12,27 +12,38 @@ const {
   color = '',
   size = 'lg',
   title,
-  modelValue = false,
+  modelValue: initialOpen = false,
 } = defineProps<Props>()
+
+// eslint-disable-next-line func-call-spacing
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: boolean):  void
+}>()
+
+const open = ref(initialOpen)
+
+watch(open, (val) => {
+  emit('update:modelValue', val)
+})
 </script>
 
 <template>
-  <MiniCard :body-class="modelValue ? 'p-4' : 'p-0'">
+  <MiniCard :body-class="open ? 'p-4' : 'p-0'">
     <template #header>
       <UButton
         :variant="variant"
         :size="size"
         :color="color"
-        :data-open="modelValue"
+        :data-open="open"
         class="w-full text-left block font-semibold m-0 data-[open=true]:rounded-b-none shadow-none"
-        @click="modelValue = !modelValue"
+        @click="open = !open"
       >
-        <UIcon name="i-heroicons-chevron-right align-text-top transition-all data-[open=true]:rotate-90" :data-open="modelValue" />
+        <UIcon name="i-heroicons-chevron-right align-text-top transition-all data-[open=true]:rotate-90" :data-open="open" />
         {{ title }}
       </UButton>
     </template>
 
-    <template v-if="modelValue" #default>
+    <template v-if="open" #default>
       <div class="rounded-t-none" v-bind="$attrs">
         <slot />
       </div>
